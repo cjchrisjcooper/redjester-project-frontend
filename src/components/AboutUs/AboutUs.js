84cf1cdf5ad9d105455f4921abfb5c695201d799
Chris Cooper <cjchrisjcooper@gmail.com>
2024-11-09 09:39:16 -0700

    feat: footer and aboutUs component

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,7 +1,19 @@
+import "./AboutUs.css";
 const AboutUs = () => {
   return (
-    <div className="main">
-      <p>About us</p>
+    <div className="AboutUs">
+      <h1 className="AboutUs__title">ABOUT US</h1>
+      <p className="AboutUs__text">
+        At Red Jester Games, we are passionate storytellers and gamers at heart,
+        committed to creating immersive worlds that captivate players and ignite
+        their imaginations. Founded in 2023, our team of experienced developers,
+        designers, and artists works collaboratively to push the boundaries of
+        gaming through innovative gameplay mechanics and stunning visuals. We
+        believe in the power of play to connect people and foster communities,
+        and each game we develop reflects our dedication to quality, creativity,
+        and player experience. Join us on this journey as we explore new
+        horizons in interactive entertainment!
+      </p>
     </div>
   );
 };
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="Footer">
-      <div>Developed by Chris</div>
-      <div>{new Date().getFullYear()}</div>
+      <div className="Footer__name">Developed by Chris</div>
+      <div className="Footer__date">{new Date().getFullYear()}</div>
     </div>
   );
 };
